44e926141af92b13c735fdb4a515eb2be12551ee
Ekaterina Filipenya <katerina.filipenua@gmail.com>
2020-04-27 15:18:05 +0300

    Add tastes enums for Main Task  (Java.Collections stage)

---
+++ src/test/java/collections/mainTask/tastes/Flavor.java
@@ -0,0 +1,5 @@
+package collections.mainTask.tastes;
+
+public enum Flavor {
+    ORANGE, RASPBERRY, STRAWBERRY, CHERRY, LEMON
+}
---
+++ src/test/java/collections/mainTask/tastes/Ingredients.java
@@ -0,0 +1,5 @@
+package collections.mainTask.tastes;
+
+public enum Ingredients {
+    NOUGAT, CARAMEL, ALMOND, HAZELNUT, WAFFLES, BISCUITS, COCONUT, LIQUOR
+}
